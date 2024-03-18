4bb92e0c5cf1eb855092cd63e165083361b67d53
sam <aquaticcalf@proton.me>
2024-03-18 23:37:25 +0530

    meow

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
-app.use(express.json());
+app.use(express.json())
 
 const authRoutes = require('./routes/auth')
 const postRoutes = require('./routes/posts')
